633b5237b3b0959e2a212b98ee61c8735e2125ca
Christoph Taucher <taucher.ch@gmail.com>
2018-12-25 14:23:59 +0100

    add Hotkey manager

---
+++ OCMHotKey/Manager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCMHotKey
+{
+    public sealed class Manager
+    {
+        #region Singleton
+        private static volatile Manager instance;
+        private static readonly object syncRoot = new Object();
+
+        private Manager() { }
+
+        public static Manager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new Manager();
+                    }
+                }
+
+                return instance;
+            }
+        }
+        #endregion
+
+        private List<HotKey> _hotKey = new List<HotKey>();
+
+        public void Add(Enums.Key key, Enums.KeyModifier keyModifiers, Func<string> callback)
+        {
+
+        }
+
+        public void Remove()
+        {
+
+        }
+
+        public void Clear()
+        {
+
+        }
+    }
+}
