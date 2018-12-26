93888c03a633ae791ba8ae6e073bac7d9c3c4e11
Christoph Taucher <taucher.ch@gmail.com>
2018-12-26 13:07:00 +0100

    add StartUp logic

--- OCMApp/App.xaml.cs
+++ OCMApp/App.xaml.cs
@@ -13,5 +13,11 @@ namespace OCMApp
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            StartUp.Instance.Init();
+        }
     }
 }
---
+++ OCMApp/OCMClipLogger.cs
@@ -0,0 +1,37 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCMApp
+{
+    public class OCMClipLogger : OCMClip.ILogger
+    {
+        public void Diagnostic(string message)
+        {
+            Log.Debug(message);
+        }
+
+        public void Error(Exception ex)
+        {
+            Log.Error(ex.ToString());
+        }
+
+        public void Error(string message)
+        {
+            Log.Error(message);
+        }
+
+        public void Info(string message)
+        {
+            Log.Information(message);
+        }
+
+        public void Warn(string message)
+        {
+            Log.Warning(message);
+        }
+    }
+}
