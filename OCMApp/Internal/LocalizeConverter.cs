3a97baeba079214d14a8fb235f4c4fc63ebdbb63
Christoph Taucher <taucher.ch@gmail.com>
2018-12-26 22:24:21 +0100

    add Localization to SettingsView and Main Window

---
+++ OCMApp/Internal/LocalizeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace OCMApp.Internal
+{
+    public class LocalizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Global.Instance.Localize.GetText(value?.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
