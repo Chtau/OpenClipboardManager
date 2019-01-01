ad75b2513eba8d7f6f44353de624d1d238e40fb4
Christoph Taucher <taucher.ch@gmail.com>
2019-01-01 14:21:43 +0100

    change Assembly settings

--- OCMApp/Properties/AssemblyInfo.cs
+++ OCMApp/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OCMApp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Open Clipboard Manager")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Christoph Taucher")]
 [assembly: AssemblyProduct("OCMApp")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Christoph Taucher Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
