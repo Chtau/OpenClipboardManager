1be192ccfe6e12eb48f96900f76f5e15b6a758fe
Christoph Taucher <taucher.ch@gmail.com>
2019-02-05 14:21:13 +0100

    add resize function for Images

--- OCMClip/ClipHandler/ConvertImage.cs
+++ OCMClip/ClipHandler/ConvertImage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
@@ -12,6 +13,36 @@ namespace OCMClip.ClipHandler
 {
     public static class ConvertImage
     {
+        public static Bitmap ResizeImage(byte[] imageByteArray, int width, int height)
+        {
+            return ResizeImage(ByteArrayToImage(imageByteArray), width, height);
+        }
+
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
         public static byte[] ImageToByteArray(System.Drawing.Image imageIn, Enums.ImageFormatType formatType)
         {
             using (MemoryStream stream = new MemoryStream())
